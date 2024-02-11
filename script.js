098b0c63492c758e092b156d46cd2b4c242993ad
khalifa-pendrops <dikee5200@gmail.com>
2024-02-11 18:50:11 +0100

    Still working on the media query and hamburger menu

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-// let hamburgerMenu = document.querySelector(".hamburger");
-// let menuBar = document.getElementById("menu");
+let hamburgerMenu = document.querySelector(".hamburger");
+let menuBar = document.getElementById("menu");
+
+hamburgerMenu.addEventListener('click', function () {
+  menuBar.classList.toggle('active');
+  console.log('you clicked me!');
+});
 
 // function toggleMenu() {
-//     // console.log(toggleMenu());
+//   // console.log(toggleMenu());
 //   hamburgerMenu.classList.toggle("active");
 //   menuBar.classList.toggle("active");
 // }

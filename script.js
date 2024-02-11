cb8fef6822d98cc1d4169377dc48605d89e53818
khalifa-pendrops <dikee5200@gmail.com>
2024-02-11 10:04:23 +0100

    javascript file added and function created. media query added and styled

---
+++ script.js
@@ -0,0 +1,8 @@
+let toggler = document.querySelector(".hamburger");
+let menuBar = document.getElementById("menu");
+
+function toggleMenu() {
+  toggler.classList.toggle("active");
+  menuBar.classList.toggle("active");
+}
+// toggleMenu();
